ed96bc1b57cf76688c128acff72b5585c1016c6a
Alex Clavel <aclavel@ocient.com>
2023-01-28 18:57:31 +0000

    Handle Query cancellation for multiple queries

--- superset/db_engine_specs/ocient.py
+++ superset/db_engine_specs/ocient.py
@@ -18,6 +18,7 @@
 import re
 
 from sqlalchemy.engine.reflection import Inspector
+from sqlalchemy.orm import Session
 from superset.db_engine_specs.base import BaseEngineSpec
 from superset.errors import SupersetErrorType
 from flask_babel import gettext as __
@@ -64,10 +65,7 @@ COLUMN_DOES_NOT_EXIST_REGEX = re.compile(
     "The reference to column '(?P<column>.*?)' is not valid"
 )
 
-# Store mapping of superset Query id -> cursor object
-# These are inserted into the cache when executing the query
-# They are then removed, either upon cancellation or query completion
-cursor_cache: Dict[Query, pyocient.Cursor]= dict()
+
 
 # Custom datatype conversion functions
 
@@ -161,6 +159,11 @@ class OcientEngineSpec(BaseEngineSpec):
     force_column_alias_quotes = True
     max_column_name_length = 30
 
+    # Store mapping of superset Query id -> cursor object
+    # These are inserted into the cache when executing the query
+    # They are then removed, either upon cancellation or query completion
+    query_id_mapping: Dict[Query, pyocient.Cursor]= dict()
+
     custom_errors : Dict[Pattern[str], Tuple[str, SupersetErrorType, Dict[str, Any]]] = {
         CONNECTION_INVALID_USERNAME_REGEX: (
             __('The username "%(username)s" does not exist.'),
@@ -244,31 +247,32 @@ class OcientEngineSpec(BaseEngineSpec):
                         row[info.column_index] = info.sanitize_func(v)
 
         # # We are done with this cursor so we can safely remove it from the cache
-        query_ids_to_delete = [q_id for q_id in cursor_cache if cursor_cache[q_id] == cursor]
+        query_ids_to_delete = [q_id for q_id in OcientEngineSpec.query_id_mapping if OcientEngineSpec.query_id_mapping[q_id] == cursor.query_id]
         for q_id in query_ids_to_delete:
-            del cursor_cache[q_id]
-        # # We are done with this cursor so we can safely remove it from the cache
-        query_ids_to_delete = [q_id for q_id in cursor_cache if cursor_cache[q_id] == cursor]
-        for q_id in query_ids_to_delete:
-            del cursor_cache[q_id]
+            del OcientEngineSpec.query_id_mapping[q_id]
+        
         return rows
 
 
     @classmethod
     def get_cancel_query_id(cls, cursor: Any, query: Query) -> Optional[str]:
-        cursor_cache[query.id] = cursor
-        
         # Return a Non-None value
         # If None is returned, Superset will not call cancel_query
         return 'DUMMY_VALUE'
 
+
+    @classmethod
+    def handle_cursor(cls, cursor: Any, query: Query, session: Session) -> None:
+        OcientEngineSpec.query_id_mapping[query.id] = cursor.query_id    
+        return super().handle_cursor(cursor, query, session)
+    
     @classmethod
     def cancel_query(cls, cursor: Any, query: Query, cancel_query_id: str) -> bool:
 
-        if query.id in cursor_cache:
-            cursor.execute(f'CANCEL {cursor_cache[query.id].query_id}')
+        if query.id in OcientEngineSpec.query_id_mapping:
+            cursor.execute(f'CANCEL {OcientEngineSpec.query_id_mapping[query.id]}')
             # Query has been cancelled, so we can safely remove the cursor from the cache
-            del cursor_cache[query.id]
+            del OcientEngineSpec.query_id_mapping[query.id]
             
             return True
         # If the query is not in the cache, it must have either been cancelled elsewhere or completed
